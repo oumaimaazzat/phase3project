b05c02aed2b94134e83c5b26d4b8b16b0d35437d
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-26 10:19:11 -0500

    client_side_monday

--- clients/javascript.js
+++ clients/javascript.js
@@ -9,12 +9,51 @@ function getWizard(){
 }
 
 function renderWizard(wizard){
-    document.getElementById("wizard-container").innerHTML += 
-    `<img src=${wizard.image} class="card-img-top" alt="...">
-                    <h5 class="card-title">${wizard.name}</h5>
-                    <p class="card-text"><small class="text-muted">Age: ${wizard.age}</small></p>
-                    <p class="card-text"><small class="text-muted">Blood: ${wizard.blood}</small></p>
-    `
+    let container = document.getElementById("wizard-container")
 
-        
+    let div1 = document.createElement('div')
+    div1.classList.add("col-sm", "m-2")
+
+    let div2 = document.createElement('div')
+    div2.className = "card"
+
+    let img = document.createElement('img')
+    img.src = wizard.image
+    img.className = 'card-img-top'
+
+    let div3 = document.createElement('div')
+    div3.className = "card-body"
+
+    let h5 = document.createElement('h5')
+    h5.className = "card-title"
+    h5.innerText = wizard.name
+
+    let p1 = document.createElement('p')
+    p1.className = 'card-text'
+    p1.innerText = wizard.age
+
+    let p2 = document.createElement('p')
+    p2.className = 'card-text'
+    p2.innerText = wizard.blood
+
+
+    container.append(div1)
+    div1.appendChild(div2)
+    div2.append(img, div3)
+    div3.append(h5, p1, p2)
+
 }
+
+
+
+
+    // `<div class="col-sm m-2">
+            // <div class="card">
+                // <img src=${cat.image} class="card-img-top" alt="...">
+        //         <div class="card-body">
+        //                 // <h5 class="card-title">${cat.name}</h5>
+        //                 // <p class="card-text"><small class="text-muted">Breed: ${cat.breed}</small></p>
+        //         </div>
+        //     </div>
+        // </div>`
+
